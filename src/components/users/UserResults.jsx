00ceec7445db50666f0c852a8cfb2545c8ab2bef
Zaidi <zaidiahsan.r@gmail.com>
2023-09-16 12:06:33 -0500

    user logins and avatars

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,32 +1,47 @@
 import { useEffect, useState } from "react";
 import Spinner from "../layout/Spinner";
+import UserItem from "./UserItem";
 
 function UserResults() {
-    const [users, setUsers] = useState([]);
+    const [users, setUsers] = useState(null); // Initialize with null
     const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null); // Add error state
 
     useEffect(() => {
         fetchUsers()
     }, []);
 
     const fetchUsers = async () => {
-        const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
-            headers: {
-                Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`
-            }
-        });
-        
-        const data = await response.json()
+        try {
+            const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
+                headers: {
+                    Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`
+                }
+            });
 
-        setUsers(data);
-        setLoading(false);
+            if (!response.ok) {
+                throw new Error(`API request failed with status: ${response.status}`);
+            }
+
+            const data = await response.json()
+
+            setUsers(data);
+            setLoading(false);
+        } catch (error) {
+            setError(error.message);
+            setLoading(false);
+        }
     };
 
-    if (!loading) {
+    if (error) {
+        return <div>Error: {error}</div>; // Display error message
+    }
+
+    if (!loading && users !== null) { // Check if users is not null
         return (
             <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
                 {users.map((user) => (
-                    <h3 key={user.id}>{user.login}</h3>
+                    <UserItem key={user.id} user={user} />
                 ))}
             </div>
         )
