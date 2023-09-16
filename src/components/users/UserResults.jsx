c7c3252cca0b8ccd9affe341dacffac16c0935ca
Zaidi <zaidiahsan.r@gmail.com>
2023-09-16 13:23:43 -0500

    creating github context to manage state

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,52 +1,26 @@
-import { useEffect, useState } from "react";
+import { useEffect, useContext } from "react";
 import Spinner from "../layout/Spinner";
 import UserItem from "./UserItem";
+import GithubContext from "../../context/github/GithubContext";
 
 function UserResults() {
-    const [users, setUsers] = useState(null); // Initialize with null
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null); // Add error state
+    const {users, loading, fetchUsers} = useContext(GithubContext)
 
     useEffect(() => {
         fetchUsers()
-    }, []);
+    });
 
-    const fetchUsers = async () => {
-        try {
-            const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
-                headers: {
-                    Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`
-                }
-            });
 
-            if (!response.ok) {
-                throw new Error(`API request failed with status: ${response.status}`);
-            }
-
-            const data = await response.json()
-
-            setUsers(data);
-            setLoading(false);
-        } catch (error) {
-            setError(error.message);
-            setLoading(false);
-        }
-    };
-
-    if (error) {
-        return <div>Error: {error}</div>; // Display error message
-    }
-
-    if (!loading && users !== null) { // Check if users is not null
-        return (
-            <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
-                {users.map((user) => (
-                    <UserItem key={user.id} user={user} />
-                ))}
-            </div>
-        )
-    } else {
-        return <Spinner />
+        if (!loading) {
+            return (
+                <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
+                    {users.map((user) => (
+                        <UserItem key={user.id} user={user} />
+                    ))}
+                </div>
+            )
+        } else {
+            return <Spinner />
     }
 }
 
